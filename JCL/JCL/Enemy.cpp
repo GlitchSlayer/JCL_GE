c47fb68f671d8774dbd7911e3792fe6bd0f7a497
InKronos - Paweł <i15.piorkowski@gmail.com>
2018-01-27 22:52:03 +0100

    Improvined class enemy and Updated it with update method.

--- JCL/JCL/Enemy.cpp
+++ JCL/JCL/Enemy.cpp
@@ -5,8 +5,9 @@
 
 
 Enemy::Enemy(std::string _name, sf::Vector2f _pos)
+	:m_name(_name), m_position(_pos), m_speed(20)
 {
-	m_shape.setPosition(_pos);
+	m_shape.setPosition(m_position);
 }
 
 
@@ -22,5 +23,23 @@ void Enemy::draw(sf::RenderWindow& window) {
 	window.draw(m_shape);
 }
 void Enemy::update(const float& deltaTime) {
+	Cannon superCannon({800, 100}, { 100, 50 });
+	sf::Vector2f cannon_position = superCannon.getPosition();
+	float dirX = cannon_position.x - m_position.x;
+	float dirY = cannon_position.y - m_position.y;
 
+	float length = std::sqrt(dirX*dirX + dirY * dirY);
+
+	if(length != 0) {
+		dirX /= length;
+		dirY /= length;
+	}
+	else {
+		dirX = 0;
+		dirY = 0;
+	}
+	m_position.x += dirX * m_speed * deltaTime;
+	m_position.y += dirY * m_speed * deltaTime;
+	m_shape.setPosition(m_position);
+	//std::cout << cannon_position.x << " ";
 }
